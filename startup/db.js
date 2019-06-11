597336d197da55a70bd9af534c66c2d5e4495863
Eyob Tefera <eyob.negussie@gmail.com>
2019-06-11 10:56:50 -0400

    added some file for deployment

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const config = require('config');
 const mongoose = require('mongoose');
 
 module.exports = async function () {
-    await mongoose.connect(`mongodb://${config.get('db.host')}/${config.get('db.database')}`, {
+    await mongoose.connect(`${config.get('db')}`, {
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
