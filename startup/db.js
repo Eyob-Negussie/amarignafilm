5f8717efbac10a5ca4c9f70ec7db4b5497211c64
Eyob Tefera <eyob.negussie@gmail.com>
2019-06-11 11:33:46 -0400

    added some file for deployment

--- startup/db.js
+++ startup/db.js
@@ -8,8 +8,5 @@ module.exports = function () {
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
-    }).then(() => {
-        winston.info(`Connected to ${db}...`);
-        console.log(`Connected to ${db}...`);
-    });
+    }).then(() => winston.info(`Connected to ${db}...`));
 }
