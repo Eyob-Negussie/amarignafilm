f9174aa7ecef570fce492780b343d4c841bae0c7
Eyob Tefera <eyob.negussie@gmail.com>
2019-07-03 18:19:24 -0400

    added username and email to jwt token

--- models/user.js
+++ models/user.js
@@ -1,47 +1,59 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255
-    },
-    isAdmin: Boolean
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  isAdmin: Boolean
 });
 
-userSchema.methods.generateAuthToken = function () {
-    return jwt.sign({id: this._id, isAdmin: this.isAdmin}, config.get('jwtPrivateKey'));
-}
+userSchema.methods.generateAuthToken = function() {
+  return jwt.sign(
+    { id: this._id, isAdmin: this.isAdmin, name: this.name, email: this.email },
+    config.get("jwtPrivateKey")
+  );
+};
 
-const User = mongoose.model('User', userSchema);
-
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(5).max(50).required(),
-        email: Joi.string().min(5).max(50).required().email(),
-        password: Joi.string().min(5).max(255).required(),
-        isAdmin: Joi.boolean()
-    };
+  const schema = {
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(50)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+    isAdmin: Joi.boolean()
+  };
 
-    return  Joi.validate(user, schema);
+  return Joi.validate(user, schema);
 }
 
 module.exports.User = User;
