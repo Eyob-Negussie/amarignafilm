0c9157989d4f15641f5198b530273879fafcda23
Eyob Tefera <eyob.negussie@gmail.com>
2019-07-03 20:00:28 -0400

    added the ability to return movie by ID

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,74 +1,78 @@
-const {Movie, validate} = require('../models/movie');
-const {Genre} = require('../models/genres');
+const { Movie, validate } = require("../models/movie");
+const { Genre } = require("../models/genres");
 
 async function getMovies() {
-    return await Movie.find().sort('title');
+  return await Movie.find().sort("title");
+}
+
+async function getMovie(id) {
+  return await Movie.findOne({ _id: id });
 }
 
 async function addMovies(movie) {
-    const {error} = validate(movie);
-    if(error){
-        throw new Error(error.details[0].message);
-        return;
-    }
+  const { error } = validate(movie);
+  if (error) {
+    throw new Error(error.details[0].message);
+    return;
+  }
 
-    const genre = await Genre.findById(movie.genreId);
-    if(!genre){
-        throw new Error(error.details[0].message);
-        return;
-    }
-    movie.genre = {
-        _id: genre._id,
-        genre: genre.genre
-    }
-    delete movie.genreId;
+  const genre = await Genre.findById(movie.genreId);
+  if (!genre) {
+    throw new Error(error.details[0].message);
+    return;
+  }
+  movie.genre = {
+    _id: genre._id,
+    genre: genre.genre
+  };
+  delete movie.genreId;
 
-    const newMovie = new Movie(movie);
+  const newMovie = new Movie(movie);
 
-    return await newMovie.save();
+  return await newMovie.save();
 }
 
 async function updateMovies(id, movie) {
-    const {error} = validate(movie);
-    if(error){
-        throw new Error(error.details[0].message);
-        return;
-    }
+  const { error } = validate(movie);
+  if (error) {
+    throw new Error(error.details[0].message);
+    return;
+  }
 
-    const genre = await Genre.findById(movie.genreId);
-    if(!genre){
-        throw new Error(error.details[0].message);
-        return;
-    }
+  const genre = await Genre.findById(movie.genreId);
+  if (!genre) {
+    throw new Error(error.details[0].message);
+    return;
+  }
 
-    movie.genre = {
-        _id: genre._id,
-        genre: genre.genre
-    }
-    delete movie.genreId;
+  movie.genre = {
+    _id: genre._id,
+    genre: genre.genre
+  };
+  delete movie.genreId;
 
-    const updatedMovie = await Movie.findOneAndUpdate(id, movie, {new: true});
-    if(!updatedMovie){
-        throw new Error('The movie with the given ID was not found.');
-        return;
-    }
+  const updatedMovie = await Movie.findOneAndUpdate(id, movie, { new: true });
+  if (!updatedMovie) {
+    throw new Error("The movie with the given ID was not found.");
+    return;
+  }
 
-    return updatedMovie;
+  return updatedMovie;
 }
 
-
 async function deleteMovies(id) {
-    const movie = await Movie.findByIdAndRemove(id);
+  const movie = await Movie.findByIdAndRemove(id);
 
-    if(!movie){
-        throw new Error('The movie with the given ID was not found.');
-        return;
-    }
+  if (!movie) {
+    throw new Error("The movie with the given ID was not found.");
+    return;
+  }
 
-    return movie;
+  return movie;
 }
 
 module.exports.getMovies = getMovies;
+module.exports.getMovie = getMovie;
 module.exports.addMovies = addMovies;
 module.exports.updateMovies = updateMovies;
 module.exports.deleteMovies = deleteMovies;
--- routes/movies.js
+++ routes/movies.js
@@ -1,26 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const movies = require('../controllers/movie');
-const auth = require('../middleware/auth');
+const movies = require("../controllers/movie");
+const auth = require("../middleware/auth");
 
-router.get('/', async (req, res) => {
-    const result = await movies.getMovies();
-    res.send(result);
+router.get("/", async (req, res) => {
+  const result = await movies.getMovies();
+  res.send(result);
 });
 
-router.post('/', auth, async (req, res) => {
-    const result = await movies.addMovies(req.body);
-    res.send(result);
+router.get("/:id", async (req, res) => {
+  const result = await movies.getMovie(req.params.id);
+  res.send(result);
 });
 
-router.put('/:id', auth, async (req, res) => {
-    const result = await movies.updateMovies(req.params.id, req.body);
-    res.send(result);
+router.post("/", auth, async (req, res) => {
+  const result = await movies.addMovies(req.body);
+  res.send(result);
 });
 
-router.delete('/:id', auth, async (req, res) => {
-    const result = await movies.deleteMovies(req.params.id);
-    res.send(result);
+router.put("/:id", auth, async (req, res) => {
+  const result = await movies.updateMovies(req.params.id, req.body);
+  res.send(result);
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  const result = await movies.deleteMovies(req.params.id);
+  res.send(result);
 });
 
 module.exports = router;
