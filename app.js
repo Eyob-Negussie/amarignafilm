33e48c60a0343b05958720e2d85a7fde70b575eb
Eyob Tefera <eyob.negussie@gmail.com>
2019-09-04 14:03:34 -0400

    changed the port

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('./startup/logging')();
-require('./startup/route')(app);
-require('./startup/db')();
-require('./startup/config')();
-require('./startup/prod')(app);
-require('./startup/validation')();
+require("./startup/logging")();
+require("./startup/route")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/prod")(app);
+require("./startup/validation")();
 
-const port = process.env.PORT || 4000;
-const server = app.listen(port, () => console.log(`Listening on port ${port} ...`));
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () =>
+  console.log(`Listening on port ${port} ...`)
+);
 
 module.exports = server;
