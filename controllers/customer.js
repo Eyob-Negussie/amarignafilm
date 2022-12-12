a2f552585a8c5dcf050a6275bdb13c6f69be42ae
Eyob Tefera <eyob.negussie@gmail.com>
2022-12-12 14:31:21 -0700

    first commit

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,47 +1,48 @@
-const{Customer, validate} = require('../models/customer');
+const { Customer, validate } = require("../models/customer");
 
 async function getCustomer() {
-    return await Customer.find().sort('firstName');
+  return await Customer.find().sort("firstName");
 }
 
 async function addCustomer(customer) {
-    const {error} = validate(customer);
-    if(error){
-        throw new Error(error.details[0].message);
-        return;
-    }
+  const { error } = validate(customer);
+  if (error) {
+    throw new Error(error.details[0].message);
+    return;
+  }
 
-    const newCustomer = new Customer(customer);
+  const newCustomer = new Customer(customer);
 
-    return await newCustomer.save();
+  return await newCustomer.save();
 }
 
 async function updateCustomer(id, customer) {
-    const {error} = validate(customer);
-    if(error){
-        throw new Error(error.details[0].message);
-        return;
-    }
+  const { error } = validate(customer);
+  if (error) {
+    throw new Error(error.details[0].message);
+    return;
+  }
 
-    const updatedCustomer = await Customer.findOneAndUpdate(id, customer, {new: true});
-    if(!updatedCustomer){
-        throw new Error('The customer with the given ID was not found.');
-        return;
-    }
+  const updatedCustomer = await Customer.findOneAndUpdate(id, customer, {
+    new: true,
+  });
+  if (!updatedCustomer) {
+    throw new Error("The customer with the given ID was not found.");
+    return;
+  }
 
-    return updatedCustomer;
+  return updatedCustomer;
 }
 
-
 async function deleteCustomer(id) {
-    const customer = await Customer.findByIdAndRemove(id);
+  const customer = await Customer.findByIdAndRemove(id);
 
-    if(!customer){
-        throw new Error('The customer with the given ID was not found.');
-        return;
-    }
+  if (!customer) {
+    throw new Error("The customer with the given ID was not found.");
+    return;
+  }
 
-    return customer;
+  return customer;
 }
 
 module.exports.getCustomer = getCustomer;
